e7221dd04e5c9aec1f5e74b91d686d88acb49208
Atulsaini2806 <atulsaini10123@gamil.com>
2025-11-20 22:44:33 +0530

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import { LOGO_URL } from "../utils/constant";
-import {useState} from "react";
+import {useState } from "react";
+import {Link} from "react-router-dom";
 
 const Header = () => {
-
   const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  // console.log("Header Render");
+  // dependency array is not mandatory inside the useEffect but callback function is mandatory.
+  // if there is no dependency array -> useEffect is called or callback function will be executed after every render.
+  // if dependency array is empty -> useEffect is called or callback function will be executed only once after initial render.
+  // if dependency array has some variables -> useEffect is called or callback function will be executed whenever the dependency variable changes.
+  // or if dependency variable is [btnNameReact] -> called everytime btnNameReact is updated .
+  // useEffect(() => {
+  //   console.log("useEffect called");
+  // }, [btnNameReact]);
 
   return (
     <div className="header">
@@ -16,9 +26,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+             <Link to ="/about">About Us</Link>
+          </li>
+          <li> 
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button 
           className="login"
