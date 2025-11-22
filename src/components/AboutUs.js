0bc2509e091d9fa21531c3736219ac9146815273
Atulsaini2806 <atulsaini10123@gamil.com>
2025-11-22 10:54:32 +0530

    About Us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,34 @@
-const AboutUs = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <h2>This is the about us page.</h2>
-    </div>
-  );
-};
+import User from "./User";
+import UserClass from "./UserClass";
+import {Component} from "react";
+
+
+class AboutUs extends Component {
+
+  constructor(props) {
+    super(props);
+    //console.log("Parent constructor");
+  }
+
+
+  componentDidMount(){
+        //console.log("Parent componentDidMount");
+        //Best place to make API calls
+    }
+
+
+  render() {
+    //console.log("Parent render");
+    
+    return (
+      <div>
+        <h1>About class Component</h1>
+        <h2>This is the about us page.</h2>
+
+        <UserClass name={"Atul Saini(classes)"} location={"Meerut(class)"} />
+      </div>
+    );
+  }
+}
 
 export default AboutUs;
