a682ffd602c26aa7f725786145c4621d821bf08a
Atulsaini2806 <atulsaini10123@gamil.com>
2025-11-22 18:02:45 +0530

    useRestaurantMenu

---
+++ src/utils/useRestaurantMenu.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import MenuData from "./MenuData";
+
+const useRestaurantMenu = (resId) =>{
+    const [resInfo, setResInfo] = useState(null);
+
+    useEffect(() => {
+    // Set MenuData only once when component mounts
+     console.log(MenuData.data);
+    
+    setResInfo(MenuData.data);
+   }, []); // ✅ empty dependency array → runs once
+
+
+    return resInfo;
+}
+
+export default useRestaurantMenu;
