696116370ef920f308cc51470cda6c485c0c979a
Atulsaini2806 <atulsaini10123@gamil.com>
2025-11-22 18:00:47 +0530

    Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import { useState,useEffect} from "react";
 // import resList from "../utils/mockData";
 import Shimmer from "./Shimmer";
 import {Link} from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 
 const Body = () => {
@@ -32,6 +33,14 @@ const Body = () => {
     setFilteredRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants );
  };
 
+const onlineStatus = useOnlineStatus();
+
+if(onlineStatus === false)
+  return ( 
+  <h1>
+    Looks like you're Offline!! Please check your internet connection;
+    </h1>
+  )
 
 
   return listOfRestaurants.length === 0 ? (
