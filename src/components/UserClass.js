28d48be2a085a4816094ca5ab55d58dd80b6dd45
Atulsaini2806 <atulsaini10123@gamil.com>
2025-11-22 10:54:57 +0530

    userClass.js

---
+++ src/components/UserClass.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+class UserClass extends React.Component{
+    constructor(props){
+        super(props);
+        
+       this.state = {
+        userInfo : { 
+            name : "Dummy Name",
+         location : "Dummy Location",
+         avatar_url : "https://avatars.githubusercontent.com/u/9919?v=4"
+        }
+       }
+       
+       //console.log("child constructor ");
+    }
+
+    async componentDidMount(){
+        //console.log("child componentDidMount");
+
+        const data = await fetch("https://api.github.com/users/ujjwalgarg-byt");
+        const json = await data.json();
+        
+        this.setState({
+            userInfo : json,
+        })
+    }
+
+    render(){
+    
+        //console.log("child render");
+        const {name, location, avatar_url} = this.state.userInfo; 
+        return(
+        <div className="user-card">
+            <img src={avatar_url} />
+            <h2>Name : {name}</h2>
+            <h2>Location : {location}</h2>
+            <h2>Contact : @atul.com</h2>
+        </div>
+        )
+    }
+}
+export default UserClass;
