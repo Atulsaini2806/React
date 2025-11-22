ae26563734d0b937a0f5f9421e202672bd23a428
Atulsaini2806 <atulsaini10123@gamil.com>
2025-11-22 10:55:28 +0530

    User.js

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+        <div className="user-card">
+            <h2>Name : Atul Saini</h2>
+            <h2>Location : Meerut</h2>
+            <h2>Contact : @atul.com</h2>
+        </div>
+    )
+}
+
+export default User;
