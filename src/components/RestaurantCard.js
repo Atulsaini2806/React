20997ca3612289b3cb28090cf98bf72cd586586d
Atulsaini2806 <atulsaini10123@gamil.com>
2025-11-27 16:12:20 +0530

    Restaurant Card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,11 +1,15 @@
+import { useContext } from "react";
 import { CDN_URL } from "../utils/constant";
-
+import UserContext from "../utils/UserContext";
 
 const RestaurantCard = ({ resData }) => {
+
+  const {loggedInUser} = useContext(UserContext);
+
   const { name, cuisines, avgRating, costForTwo,sla, cloudinaryImageId } = resData.info;
 
   return (
-    <div className="m-3 p-3 w-[200px] rounded-lg bg-gray-200 hover:bg-gray-300">
+    <div className="m-3 p-3 w-[200px] h-[450px] rounded-lg bg-gray-200 hover:bg-gray-300">
       <img
         className="rounded-lg"
         alt={name}
@@ -16,8 +20,24 @@ const RestaurantCard = ({ resData }) => {
       <h4>{avgRating} ⭐</h4>
       <h4>{costForTwo}</h4>
       <h4>{sla.deliveryTime} mins</h4>
+       <h4>User : {loggedInUser} </h4> 
     </div>
   );
 };
+
+//higher order function
+// inout - RestaurantCard =>> RestaurantCardWithpure veg
+
+export const withPureVeg = (RestaurantCard) => {
+  return (props) => {
+    return(
+      <div className="relative">
+      <label className="bg-green-500 rounded-lg p-1 absolute top-0 left-0 z-10  text-white ">Pure Veg</label>
+      <RestaurantCard {...props} />
+      </div>
+    )
+  };
+};
+
 
 export default RestaurantCard;
